b41a36bf1618536cbc134cbbf20c1d0b21586b58
JoaquinMonge <joamonb@gmail.com>
2023-01-23 21:49:15 -0600

    account created succesfully

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,18 +1,22 @@
-import React, { useState } from "react";
+import React, { createContext, useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth, db } from "../firebase";
 
 import "./SignUp.css";
 import { doc, setDoc } from "firebase/firestore";
+import UserContext from "../UserContext";
+import { Link } from "react-router-dom";
 export const SignUp = () => {
+  const { authUser } = createContext(UserContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
 
   const handleSignUp = (e) => {
     e.preventDefault();
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
+        setSuccess(true);
         console.log(userCredential);
         setDoc(doc(db, "users", email), {
           cartItems: [],
@@ -23,21 +27,28 @@ export const SignUp = () => {
       });
   };
   return (
-    <form onSubmit={handleSignUp} className="signin">
-      <h2 className="title">Create Account</h2>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button className="signinbtn">Sign Up</button>
-    </form>
+    <>
+      {success ? (
+        <p className="user">Account created succesfully</p>
+      ) : (
+        <form onSubmit={handleSignUp} className="signin">
+          <h2 className="title">Create Account</h2>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button className="signinbtn">Sign Up</button>
+        </form>
+      )}
+    </>
   );
 };
